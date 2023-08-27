ac5a53dca693d54209b99060b8296ac0a390b754
Marina Nikulina <bekina_marina@mail.ru>
2023-08-28 00:12:34 +0700

    refactor: вынести валидацию celebrate в отдельный файл

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config({ path: "./config/.env" });
-const { celebrate, Joi } = require("celebrate");
 const { errors } = require("celebrate");
 const cookieParser = require("cookie-parser");
 const auth = require("./middlewares/auth");
 const { createUser, login } = require("./controllers/users");
 const errorHandler = require("./middlewares/error-handler");
-const { REGEX } = require("./utils/constants");
 
 const { PORT = 3000, MONGODB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
   process.env;
 
 const userRouter = require("./routes/users");
 const cardRouter = require("./routes/cards");
+const {
+  validationSchemaSignIn,
+  validationSchemaSignup,
+} = require("./middlewares/validation");
 
 const NotFoundError = require("./errors/not-found-error");
 
@@ -33,31 +35,8 @@ app.use(cookieParser());
 
 app.use(bodyParser.json());
 
-app.post(
-  "/signin",
-  celebrate({
-    body: Joi.object()
-      .keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required(),
-      })
-      .unknown(true),
-  }),
-  login,
-);
-app.post(
-  "/signup",
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(REGEX),
-    }),
-  }),
-  createUser,
-);
+app.post("/signin", validationSchemaSignIn, login);
+app.post("/signup", validationSchemaSignup, createUser);
 
 // Защита API авторизацией
 app.use(auth);
